ef0ef7649e06c3974ec894f085abdf62c1482677
Ankur77720 <prajapatiankur8349@gmail.com>
2024-11-24 20:30:07 +0530

    complete

--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -3,6 +3,7 @@ import { Link, useLocation } from 'react-router-dom'
 import FinishRide from '../components/FinishRide'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import LiveTracking from '../components/LiveTracking'
 
 const CaptainRiding = () => {
 
@@ -27,7 +28,7 @@ const CaptainRiding = () => {
 
 
     return (
-        <div className='h-screen relative'>
+        <div className='h-screen relative flex flex-col justify-end'>
 
             <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
                 <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
@@ -35,10 +36,7 @@ const CaptainRiding = () => {
                     <i className="text-lg font-medium ri-logout-box-r-line"></i>
                 </Link>
             </div>
-            <div className='h-4/5'>
-                <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
 
-            </div>
             <div className='h-1/5 p-6 flex items-center justify-between relative bg-yellow-400 pt-10'
                 onClick={() => {
                     setFinishRidePanel(true)
@@ -50,10 +48,14 @@ const CaptainRiding = () => {
                 <h4 className='text-xl font-semibold'>{'4 KM away'}</h4>
                 <button className=' bg-green-600 text-white font-semibold p-3 px-10 rounded-lg'>Complete Ride</button>
             </div>
-            <div ref={finishRidePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+            <div ref={finishRidePanelRef} className='fixed w-full z-[500] bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
                 <FinishRide
                     ride={rideData}
                     setFinishRidePanel={setFinishRidePanel} />
+            </div>
+
+            <div className='h-screen fixed w-screen top-0 z-[-1]'>
+                <LiveTracking />
             </div>
 
         </div>
--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -3,6 +3,7 @@ import { Link, useLocation } from 'react-router-dom' // Added useLocation
 import { useEffect, useContext } from 'react'
 import { SocketContext } from '../context/SocketContext'
 import { useNavigate } from 'react-router-dom'
+import LiveTracking from '../components/LiveTracking'
 
 const Riding = () => {
     const location = useLocation()
@@ -21,7 +22,7 @@ const Riding = () => {
                 <i className="text-lg font-medium ri-home-5-line"></i>
             </Link>
             <div className='h-1/2'>
-                <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+                <LiveTracking />
 
             </div>
             <div className='h-1/2 p-4'>
